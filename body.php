4b89784501a73d8587d71bdce7c732ff580e5d06
RobertoHanus <robertohanus@hotmail.com>
2023-02-20 14:50:53 -0300

    Working on frontend

--- body.php
+++ body.php
@@ -1,37 +1,43 @@
-<style>
-    label {
-        margin-left: 20px;
-    }
-
-    #datepicker {
-        width: 180px;
-    }
-
-    #datepicker>span:hover {
-        cursor: pointer;
-    }
-</style>
-
-
 <div class="container">
-    <h1 class="text-success font-weight-bold">
-        GeeksforGeeks
+    <h1 class="font-weight-bold">
+        CRUD e Importador de datos desde API
+        <i class="glyphicon glyphicon-calendar"></i>
     </h1>
-    <h3>
-        setting up bootstrap datepicker
-    </h3>
-    <label>Select Date: </label>
-    <div id="datepicker" class="input-group date" data-date-format="dd-mm-yyyy">
-        <input class="form-control" type="text" readonly />
-        <span class="input-group-addon">
-            <i class="glyphicon glyphicon-calendar"></i>
-        </span>
+    <div class="row border mb-3">
+        <button type="button" class="btn btn-success m-3">Importar Datos</button>
+        <button type="button" class="btn btn-danger m-3">Borrar Todo</button>
+    </div>
+    <div class="row border p-2 mb-3">
+    </div>
+    <div class="row border p-2">
+        <div class="col-6 col-sm-4 col-md-3">
+            <label>Fecha inicio: </label>
+            <div id="datepicker-start" class="input-group date" data-date-format="dd-mm-yyyy">
+                <input class="form-control" type="text" readonly />
+                <span class="input-group-addon">
+                </span>
+            </div>
+        </div>
+        <div class="col-6 col-sm-4 col-md-3">
+            <label>Fecha final: </label>
+            <div id="datepicker-end" class="input-group date" data-date-format="dd-mm-yyyy">
+                <input class="form-control" type="text" readonly />
+                <span class="input-group-addon">
+                </span>
+            </div>
+        </div>
     </div>
 </div>
 
 <script>
     $(function () {
-        $("#datepicker").datepicker({
+        $("#datepicker-start").datepicker({
+            autoclose: true,
+            todayHighlight: true,
+        }).datepicker('update', new Date());
+    });
+    $(function () {
+        $("#datepicker-end").datepicker({
             autoclose: true,
             todayHighlight: true,
         }).datepicker('update', new Date());
--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php 
 include('header.php');
 include('body.php');
-include('scripts.php');
 include('bottom.php');
 ?>
