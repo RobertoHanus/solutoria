50552e536bdfa8a74149d23ddc9f1c5d8ddfdd25
RobertoHanus <robertohanus@hotmail.com>
2023-02-23 06:25:59 -0300

    Almost done!

--- test.php
+++ test.php
@@ -1,72 +1,48 @@
-<!DOCTYPE html>
-<html lang="en">
-  
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" 
-          content="width=device-width, 
-                   initial-scale=1.0">
-    <link href=
-"https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" 
-          rel="stylesheet" type="text/css" />
-    <link rel="stylesheet" href=
-"https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css">
-    <title>Bootstrap Datepicker</title>
-    <style>
-      label{margin-left: 20px;}
-      #datepicker{width:180px;}
-      #datepicker > span:hover{cursor: pointer;}
-    </style>
- </head>
-  
-<body>
-    <div class="container">
-        <h1 class="text-success font-weight-bold">
-            GeeksforGeeks
-        </h1>
-        <h3>
-            setting up bootstrap datepicker
-        </h3>
-        <label>Select Date: </label>
-        <div id="datepicker" 
-             class="input-group date" 
-             data-date-format="mm-dd-yyyy">
-            <input class="form-control" 
-                   type="text" readonly />
-            <span class="input-group-addon">
-                <i class="glyphicon glyphicon-calendar"></i>
-            </span>
-        </div>
-    </div>
-  
-    <script src="https://code.jquery.com/jquery-3.6.1.min.js" 
-        integrity=
-    "sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" 
-        crossorigin="anonymous">
-    </script>
-    <script>
-        $(function () {
-            $("#datepicker").datepicker({ 
-                autoclose: true, 
-                todayHighlight: true,
-            }).datepicker('update', new Date());
-        });
-    </script>
-    <script src=
-"https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" 
-        integrity=
-"sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" 
-        crossorigin="anonymous">
-    </script>
-    <script src=
-"https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" 
-        integrity=
-"sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" 
-        crossorigin="anonymous">
-    </script>
-    <script src=
-"https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js">
-    </script>
-</body>
-  
-</html>
+<?php
+$ch  = curl_init();
+
+$data = array("userName" => "robertohanus4_94p@indeedemail.com", "flagJson" => true );
+
+$postdata = json_encode($data);
+
+$options = [
+    CURLOPT_URL => 'https://postulaciones.solutoria.cl/api/acceso',
+    CURLOPT_POST => true,
+    CURLOPT_HTTPHEADER => ["Accept: application/json", "Content-Type: application/json"],
+    CURLOPT_POSTFIELDS => $postdata,
+    CURLOPT_RETURNTRANSFER => true
+ ];
+
+ curl_setopt_array($ch, $options);
+
+$result = curl_exec($ch);
+
+$decoded = json_decode($result, true);
+
+$token = $decoded['token'];
+
+curl_close($ch);
+
+$ch  = curl_init();
+
+$options = [
+    CURLOPT_URL => 'https://postulaciones.solutoria.cl/api/indicadores',
+    CURLOPT_POST => false,
+    CURLOPT_HTTPHEADER => ["Accept: application/json", "Content-Type: application/json", "Authorization: Bearer $token"],
+    CURLOPT_RETURNTRANSFER => true
+ ];
+
+curl_setopt_array($ch, $options);
+
+$resultData = curl_exec($ch);
+
+$decodedData = json_decode($resultData, true);
+
+// var_dump($decodedData[0]);
+
+foreach($decodedData as $row) {
+    if($row['codigoIndicador'] == "UF") var_dump($row);
+}
+
+curl_close($ch);
+?>
